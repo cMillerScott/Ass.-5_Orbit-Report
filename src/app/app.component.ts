697cfd29664f52bcf8807ac91060784a41bd23bf
Christopher <c.millerscott@gmail.com>
2020-12-27 08:05:03 -0600

    stuck on sSW method

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,11 +9,11 @@ import { Satellite } from "./satellite";
 export class AppComponent {
   title = "orbit-report";
   sourceList: Satellite[];
+
   constructor() {
     this.sourceList = [];
     let satellitesUrl =
       "https://handlers.education.launchcode.org/static/satellites.json";
-
     window.fetch(satellitesUrl).then(
       function (response) {
         response.json().then(
