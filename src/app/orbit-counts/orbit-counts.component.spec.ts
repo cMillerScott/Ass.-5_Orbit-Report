a155ebf0cf08438e959e2d2917a32b8bd33a7bf9
Christopher <c.millerscott@gmail.com>
2021-07-21 11:27:31 -0500

    cli core

--- src/app/orbit-counts/orbit-counts.component.spec.ts
+++ src/app/orbit-counts/orbit-counts.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { OrbitCountsComponent } from './orbit-counts.component';
 
@@ -6,7 +6,7 @@ describe('OrbitCountsComponent', () => {
   let component: OrbitCountsComponent;
   let fixture: ComponentFixture<OrbitCountsComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ OrbitCountsComponent ]
     })
--- src/app/orbit-list/orbit-list.component.spec.ts
+++ src/app/orbit-list/orbit-list.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { OrbitListComponent } from './orbit-list.component';
 
@@ -6,7 +6,7 @@ describe('OrbitListComponent', () => {
   let component: OrbitListComponent;
   let fixture: ComponentFixture<OrbitListComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ OrbitListComponent ]
     })
